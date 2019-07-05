f649c52dace44b8387793f0025a43d36596c97af
peter okoth <pokoth2009@gmail.com>
2019-07-05 14:01:04 +0300

    first upload

--- instance/config.py
+++ instance/config.py
@@ -28,5 +28,5 @@ app_config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'release': ReleaseConfig,
-    'db_url': "postgres://qhxwbysu:Ly5l-uZICzJ-G-Z0SmVasYLsy_wtqpPa@raja.db.elephantsql.com:5432/qhxwbysu"
+    'db_url': "postgres://izimgmjdxlqico:132cdba9eabd5b4a5558319ba47d09f8fd241582e832aa4a23a92c8b9ac35401@ec2-54-83-1-101.compute-1.amazonaws.com:5432/d4vk1l424q9j3f"
 }
